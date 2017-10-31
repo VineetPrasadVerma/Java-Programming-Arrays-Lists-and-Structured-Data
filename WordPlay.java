3ad59054e19095167c48c21a2d09de5c1983b054
Vineet Prasad Verma <vineetpd1996@gmail.com>
2017-10-31 08:12:57 +0530

    Add isVowel() and tester()

--- WordPlay.java
+++ WordPlay.java
@@ -6,5 +6,46 @@
  * @version (a version number or a date)
  */
 public class WordPlay {
+    public boolean isVowel(char ch){
+        boolean test = false;  
+        String vowels = "aeiou";
+        ch =Character.toLowerCase(ch);
+        /*int currIndex = vowels.indexOf(ch);
+        if(currIndex != -1){
+        
+            return true;
+        }
+        return false;*/
+        for(int i=0; i<vowels.length(); i++){
+        
+            char message = vowels.charAt(i);
+            if(ch == message){
+                test = true;
+                break;
+            }
+        }
+        if(test == true){
+            return true;
+        }
+        else{
+           return false;
+        }
+    }
+    
+    public void tester(){
+    
+        boolean result = isVowel('f');
+        System.out.println("ANS IS = "+result);
+        
+        result = isVowel('a');
+        System.out.println("ANS IS = "+result);
+        
+        result = isVowel('F');
+        System.out.println("ANS IS = "+result);
+        
+        result = isVowel('A');
+        System.out.println("ANS IS = "+result);
+        
+    }
 
 }
